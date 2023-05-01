afa9a19804c39ad8f64668d4ff5cd391c84fa8f2
Lucas Bilharinho <49289812+LBilharinho@users.noreply.github.com>
2023-05-01 16:04:42 -0300

    first commit

--- src/test/java/automatizado/teste/GoogleTest.java
+++ src/test/java/automatizado/teste/GoogleTest.java
@@ -5,26 +5,12 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GoogleTest {
-
-    private WebDriver driver;
-    private final String URL_BASE = "https://www.google.com/";
-    private final String CAMINHO_DRIVER = "src/test/java/automatizado/resource/chromedriver-v11205615.exe";
-
-    private void iniciar(){
-        System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(URL_BASE);
-    }
+public class GoogleTest extends baseTest {
 
     @Test
     public void devePesquisarNoGoogle(){
-        iniciar();
 
         WebElement inputPesquisa = driver.findElement(By.name("q"));
         inputPesquisa.sendKeys("batata frita" + Keys.ENTER);
@@ -32,9 +18,18 @@ public class GoogleTest {
 
         assertTrue(resultado, resultado.contains("Aproximadamente"));
 
-        driver.quit();
+    }
 
+    @Test
+    public void devePesquisarNoGoogle2(){
+
+        WebElement inputPesquisa = driver.findElement(By.name("q"));
+        inputPesquisa.sendKeys("hamburguer" + Keys.ENTER);
+        String resultado =driver.findElement(By.id("result-stats")).getText();
+
+        assertTrue(resultado, resultado.contains("Aproximadamente"));
 
     }
+
     
 }
